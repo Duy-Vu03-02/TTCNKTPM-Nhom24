c6a6b1cb1e13b6160c7859ef18c431a6e8289054
Duy-Vu03-02 <duyvu11122002@gmail.com>
2024-03-23 11:15:48 +0700

    update QuestionTempalte

--- src/component/QuesitonTemplate.jsx
+++ src/component/QuesitonTemplate.jsx
@@ -3,50 +3,168 @@ import "../resources/component/questiontemplate.css";
 import { IoMdCheckmark } from "react-icons/io";
 import { LuClock } from "react-icons/lu";
 import { GoDash } from "react-icons/go";
+import { FaRegStar } from "react-icons/fa6";
 
 export default function QuesitonTemplate({ dataQuestion }) {
   const [listData, setListData] = useState([]);
-  const [currentQuestion, setCurrentQusetion] = useState(0);
+  const [timeExam, setTimeExam] = useState(1200);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [quesitons, setQuestions] = useState([]);
+  const [autoNextQs, setAutoNextQs] = useState(true);
+  const [score, setScore] = useState({
+    state: false,
+    show: false,
+    pass: false,
+    countTrue: 0,
+    countMustTrue: 0,
+    countTrueMustTrue: 0,
+  });
+  const indexQuestion = useRef(1);
 
   useEffect(() => {
-    var data = dataQuestion;
-    data.forEach((element, z) => {
-      element.selected = null;
-      element.index = z + 1;
-    });
-    setListData(data);
+    setCurrentQuestion(dataQuestion[0]);
+    const fetch = async () => {
+      var data = await dataQuestion;
+      data.forEach((item, index) => {
+        item.selected = null;
+        item.zindex = index;
+      });
+      setListData(data);
+    };
+    fetch();
   }, [dataQuestion]);
 
-  const handleShowQuestion = (index) => {
-    setCurrentQusetion(index);
+  useEffect(() => {
+    if (quesitons.length === listData.length && quesitons.length > 0) {
+      setScore((prevState) => {
+        return {
+          ...prevState,
+          state: true,
+        };
+      });
+    }
+  }, [quesitons]);
+
+  const handleShowQuestion = (data, index) => {
+    setCurrentQuestion(data);
+    indexQuestion.current = index;
+  };
+  const handleSelected = (id, z) => {
+    //handle checkbox
+    setQuestions((prevSelected) => {
+      const newSelect = [
+        {
+          id: id,
+          selected: z,
+        },
+      ];
+      const filterSelected = prevSelected.filter((item) => item.id !== id);
+      return [...newSelect, ...filterSelected];
+    });
+
+    //handle change select listdata
+    for (var item of listData) {
+      if (item.zindex === id) {
+        item.selected = z;
+      }
+    }
+
+    // handle Auto next question
+    handleAutoNextQuestion();
+  };
+  const handleCalculatorScore = () => {
+    handleTimeOut();
+    var countTrue = 0;
+    var countMustTrue = 0;
+    var countTrueMustTrue = 0;
+    listData.forEach((element) => {
+      console.log(element);
+      if (element.selected === element.trueAnswer) countTrue++;
+      if (element.mustCorrect === true) {
+        countMustTrue++;
+        if (element.selected === element.trueAnswer) {
+          countTrueMustTrue++;
+        }
+      }
+    });
+
+    setScore((prevState) => {
+      return {
+        ...prevState,
+        state: true,
+        show: true,
+        countTrue: countTrue,
+        countMustTrue: countMustTrue,
+        countTrueMustTrue: countTrueMustTrue,
+        pass:
+          countMustTrue === countTrueMustTrue &&
+          countMustTrue > 0 &&
+          countTrue >= 10
+            ? true
+            : false,
+      };
+    });
   };
 
+  const handleAutoNextQuestion = () => {
+    if (autoNextQs && indexQuestion.current < listData.length) {
+      const timeout = setTimeout(() => {
+        setCurrentQuestion(listData[indexQuestion.current]);
+        indexQuestion.current += 1;
+      }, 1000);
+      return () => clearTimeout(timeout);
+    }
+  };
+  const handleTimeOut = () => {
+    setTimeExam(0);
+  };
+  const handleChangeAutoNext = () => {
+    autoNextQs ? setAutoNextQs(false) : setAutoNextQs(true);
+  };
+  const handle = () => {};
   return (
     <>
       <div className="question-template">
         <div>
           <div className="conten-domain flex">
             <div className="left-temp">
-              {listData[currentQuestion] ? (
+              {currentQuestion ? (
                 <div className="wrap-list-question">
-                  <li key={listData[currentQuestion].id}>
+                  <li key={currentQuestion.zindex}>
                     <div className="wrap-question">
                       <div className="question-infor flex">
-                        <h5>Câu {listData[currentQuestion].id}.&nbsp;</h5>
-                        <h5>{listData[currentQuestion].question}?</h5>
+                        <h5>
+                          Câu {currentQuestion.zindex + 1}
+                          .&nbsp;
+                        </h5>
+                        <h5>{currentQuestion.question}?</h5>
                       </div>
-                      {listData[currentQuestion].img ? (
+                      {currentQuestion.img ? (
                         <div className="img-illustration">
-                          <img src={listData[currentQuestion].img} alt="" />
+                          <img src={currentQuestion.img} alt="" />
                         </div>
                       ) : (
                         ""
                       )}
                       <div className="answer-infor">
                         <ul>
-                          {listData[currentQuestion].answer.map((rep, z) => (
-                            <li key={z} className="flex">
-                              <input type="checkbox" />
+                          {currentQuestion.answer.map((rep, z) => (
+                            <li
+                              key={z}
+                              className={`${score.show ? "disable" : ""} flex`}
+                              onClick={() =>
+                                handleSelected(currentQuestion.zindex, z)
+                              }
+                            >
+                              <input
+                                type="checkbox"
+                                checked={quesitons.some(
+                                  (item) =>
+                                    item.id === currentQuestion.zindex &&
+                                    item.selected === z
+                                )}
+                                onChange={handle}
+                              />
                               <span>{z + 1}.&nbsp; </span>
                               <p>{rep}.</p>
                             </li>
@@ -63,30 +181,101 @@ export default function QuesitonTemplate({ dataQuestion }) {
             <div className="right-temp">
               <div className="timer flex">
                 <LuClock className="icon-clock" />
-                <Clock timer={1200} className="cpn" />
+                <Clock
+                  timer={timeExam}
+                  timeOut={handleTimeOut}
+                  className="cpn"
+                />
               </div>
               <div className="wrap-btn-complete">
-                <div className="btn-complete flex">
-                  <IoMdCheckmark className="icon-check" />
-                  <button>nộp bài</button>
-                </div>
+                {!score.show ? (
+                  <div className={`btn-complete flex`}>
+                    <div
+                      className="auto-next-qs"
+                      onClick={handleChangeAutoNext}
+                    >
+                      <input
+                        className={autoNextQs ? "" : "un-auto-next-qs"}
+                        type="button"
+                        value={
+                          autoNextQs ? "Tự động nhảy câu" : "Không tự nhảy câu"
+                        }
+                      />
+                      <div
+                        className={`btn-auto-next-qs ${
+                          autoNextQs ? "" : "un-auto-next-qs"
+                        }`}
+                      ></div>
+                    </div>
+                    <div
+                      className={`${score.state ? "" : "btn-not-allowed"} flex`}
+                    >
+                      <IoMdCheckmark className="icon-check" />
+                      <button
+                        onClick={handleCalculatorScore}
+                        className={score.state ? "" : "btn-not-allowed"}
+                      >
+                        nộp bài
+                      </button>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="result-exam">
+                    <div className="title-result-exam">
+                      <h3>{score.pass ? "đạt" : "không đạt"}</h3>
+                    </div>
+                    <div className="detial-result-exam">
+                      <p>
+                        Điểm đạt được: {score.countTrue}/{listData.length}
+                      </p>
+                      <p>
+                        Số câu điểm liệt sai:{" "}
+                        {score.countMustTrue - score.countTrueMustTrue}/
+                        {score.countMustTrue}
+                      </p>
+                    </div>
+                  </div>
+                )}
               </div>
               <div className="show-list">
                 <ul className="wrap-list-qs flex">
-                  {listData.map((data, index) => (
-                    <li
-                      key={index}
-                      className={`item-select ${
-                        index === currentQuestion ? "item-select-active" : ""
-                      } flex`}
-                      onClick={() => handleShowQuestion(index)}
-                    >
-                      <div className="btn-select flex">
-                        <GoDash className="icon-dash" />
-                        <p>{index + 1}</p>
-                      </div>
-                    </li>
-                  ))}
+                  {currentQuestion && listData
+                    ? listData.map((data, index) => (
+                        <li
+                          key={index}
+                          className={`item-select ${
+                            index === currentQuestion.zindex
+                              ? "item-select-active"
+                              : ""
+                          }
+                          ${data.selected !== null ? "item-select-ok" : ""}
+                          
+                          flex`}
+                          onClick={() => handleShowQuestion(data, index)}
+                        >
+                          <div
+                            className={`btn-select
+                            ${
+                              score.show
+                                ? data.selected === data.trueAnswer
+                                  ? "result-question-true"
+                                  : "result-question-false"
+                                : ""
+                            }
+                          flex`}
+                          >
+                            {listData[index].selected === null ? (
+                              <GoDash className="icon-dash" />
+                            ) : listData[index].mustCorrect && score.show ? (
+                              <FaRegStar className="icon-dash" />
+                            ) : (
+                              <IoMdCheckmark className="icon-dash" />
+                            )}
+                            <p>{index + 1}</p>
+                          </div>
+                        </li>
+                      ))
+                    : ""}
                 </ul>
               </div>
             </div>
@@ -98,17 +287,26 @@ export default function QuesitonTemplate({ dataQuestion }) {
   );
 }
 
-function Clock({ timer }) {
+function Clock({ timer, timeOut }) {
   const [timeRemaining, setTimeRemaining] = useState({
-    minute: Math.floor(timer / 60),
-    second: Math.floor(timer % 60),
+    minute: 0,
+    second: 0,
   });
   const interval = useRef(null);
+
+  useEffect(() => {
+    setTimeRemaining({
+      minute: Math.floor(timer / 60),
+      second: Math.floor(timer % 60),
+    });
+  }, [timer]);
+
   useEffect(() => {
     interval.current = setInterval(() => {
       setTimeRemaining((prevTime) => {
         if (prevTime.second === 0) {
           if (prevTime.minute < 1) {
+            timeOut();
             clearInterval(interval.current);
             return {
               minute: 0,
--- src/page/ExamRandom.jsx
+++ src/page/ExamRandom.jsx
@@ -83,23 +83,60 @@ function Exam(props) {
       "được sự cho phép của Đảng và Nhà nước",
     ],
     trueAnswer: 2,
+    mustCorrect: false,
   };
   const store1 = { ...store };
-  store1.id = 4;
+  store1.id = 1;
+  store1.mustCorrect = true;
   store1.img = null;
-
   const store2 = { ...store };
-  store2.id = 3;
-
+  store2.id = 2;
   const store3 = { ...store };
-  store3.id = 2;
-
+  store3.id = 3;
   const store4 = { ...store };
-  store4.id = 1;
+  store4.id = 4;
+  const store5 = { ...store };
+  store5.id = 5;
+  const store6 = { ...store };
+  store6.id = 6;
+  store6.mustCorrect = true;
+  const store7 = { ...store };
+  store7.id = 7;
+  const store8 = { ...store };
+  store8.id = 8;
+  store8.img = null;
+  const store9 = { ...store };
+  store9.id = 9;
+  const store10 = { ...store };
+  store10.id = 10;
+  const store11 = { ...store };
+  store11.id = 11;
+  const store12 = { ...store };
+  store12.img = null;
+  store12.id = 12;
+  const store13 = { ...store };
+  store13.id = 13;
+  const store14 = { ...store };
+  store14.id = 14;
 
   // const listData = Array.from({ length: 25 }, () => store);
-  const temp = [store, store1, store2, store3, store4];
-  const listData = [...temp, ...temp, ...temp, ...temp, ...temp];
+  const listData = [
+    store,
+    store1,
+    store2,
+    store3,
+    store4,
+    store5,
+    store6,
+    store7,
+    store8,
+    store9,
+    store10,
+    store11,
+    store12,
+    store13,
+    store14,
+  ];
 
   const handleTopic = () => {
     props.btnTopic(false);
